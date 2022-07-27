95a5dbfe15503e0bda32d9f8b6d0bf8250993924
Abriel <ravenz1@yandex.ru>
2022-07-27 22:34:56 +0300

    Убрал огрызки. Mile Stone 1.1

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         Manager taskManager = new Manager();
-        //    Epic epic_1 = new Epic(0, "Переезд", "Переезд", "NEW");
-        //    SubTask subTask = new SubTask(man.id,0,"Заказать", "Позвонить в транспортную компанию", "NEW");
-        //   Task task = new Task(0, "Забираем Коте", "Самое главное", "NEW");
         Epic e1 = new Epic("Накормить коте", "Важнейшее", "NEW");
         SubTask s1 = new SubTask("Заставить себя", "Трудно", "IN_PROGRESS", 1);
         SubTask s2 = new SubTask("Пойти в магазин", "Очиння дорого", "NEW", 1);
