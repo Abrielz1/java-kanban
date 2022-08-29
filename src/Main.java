be3b1ec08ea0876ea1d76ed27e3b99d73351fefe
Abriel <ravenz1@yandex.ru>
2022-08-30 01:01:25 +0300

    Всё кроме 1го замечание doen
    . MileStone 4.0.5

--- src/Main.java
+++ src/Main.java
@@ -12,16 +12,37 @@ public class Main {
 
         TaskManager taskManager = Managers.getDefaultTask();
 //================================================================================
-        taskManager.add(new Epic(InMemoryTaskManager.getIdCounter(), "Накормить коте", "Важнейшее", TaskStatus.NEW));
-        taskManager.add(new SubTask(InMemoryTaskManager.getIdCounter(), "Заставить себя", "Трудно", TaskStatus.NEW, 1));
-        taskManager.add(new SubTask(InMemoryTaskManager.getIdCounter(), "Пойти в магазин", "Купить корм", TaskStatus.NEW, 1));
+        taskManager.add(new Epic("Накормить коте", "Важнейшее", TaskStatus.NEW));
+        taskManager.add(new SubTask("Заставить себя", "Трудно", TaskStatus.NEW, 1));
+        taskManager.add(new SubTask("Пойти в магазин", "Купить корм", TaskStatus.NEW, 1));
+        taskManager.add(new SubTask("Купить корм", "Выбрать корм", TaskStatus.IN_PROGRESS, 1));
 //================================================================================
-        taskManager.add(new Epic(InMemoryTaskManager.getIdCounter(), "Накормить Коте", "Проверить есть ли СВЕЖАЯ вода", TaskStatus.NEW));
-        taskManager.add(new SubTask(InMemoryTaskManager.getIdCounter(), "Насыпать корм", "Успеть убежать от миски, затопчет", TaskStatus.NEW, 2));
+        taskManager.add(new Epic("Накормить Коте", "Проверить есть ли СВЕЖАЯ вода", TaskStatus.NEW));
+        taskManager.add(new SubTask("Насыпать корм", "Успеть убежать от миски, затопчет", TaskStatus.NEW, 2));
 //================================================================================
-        taskManager.add(new Task(InMemoryTaskManager.getIdCounter(), "Погладить кота", "", TaskStatus.NEW));
+        taskManager.add(new Epic("Пойти в магазин", "Покупки", TaskStatus.NEW));
 //================================================================================
-        taskManager.update(new SubTask(2, "Заставить себя", "Трудно", TaskStatus.IN_PROGRESS, 1));
+        taskManager.add(new Task("Погладить кота", "поймать его", TaskStatus.NEW));
+        taskManager.add(new Task("Убраться в доме", "заставить себя", TaskStatus.IN_PROGRESS));
+//================================================================================
+        taskManager.update(new SubTask("Заставить себя", "Трудно", TaskStatus.IN_PROGRESS, 1));
+
+        taskManager.getEpicById(1);
+        taskManager.getSubtaskById(2);
+        taskManager.getSubtaskById(2);
+        taskManager.getSubtaskById(3);
+        taskManager.getEpicById(5);
+        taskManager.getEpicById(7);
+        taskManager.getTaskById(8);
+        taskManager.getTaskById(8);
+        taskManager.getTaskById(9);
+        taskManager.removeEpicById(7);
+        taskManager.removeEpicById(1);
+
+
+
+
+
 
     }
 }
